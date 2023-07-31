c4f802396a19fcd4a44ca1bde2382ee6d741e1a8
Ruben Garcia <rubengarciaalba@gmail.com>
2023-07-31 12:28:30 +0200

    Finished API following tutorial

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,13 +1,33 @@
 import express from 'express'
+import * as diaryService from '../services/diaryService'
+import toNewDiaryEntry from '../utils';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-    res.send('Fetching all entry diaries.');
+    res.send(diaryService.getEntriesWithoutSensitiveInfo());
 });
 
-router.post('/', (_req, res) => {
-    res.send('Saving a diary!');
+router.get('/:id', (req, res) => {
+    const diary = diaryService.findById(Number(req.params.id));
+
+    return diary 
+        ? res.send(diary)
+        : res.sendStatus(404);
+});
+
+router.post('/', (req, res) => {
+    try {
+        const newDiaryEntry = toNewDiaryEntry(req.body);
+
+        const addedDiaryEntry = diaryService.addDiary(newDiaryEntry);
+
+        res.json(addedDiaryEntry);
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            res.status(400).send(e.message);
+        }
+    }    
 });
 
 export default router;
